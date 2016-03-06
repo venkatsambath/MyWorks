815c823729e5e3ee2e69e4e54e7e558ffed56622
venkatsambath <svenkataramanasam@cloudera.com>
2016-03-07 01:51:03 +0530

    Different Compression Format

--- Exercises/working_with_compressed_files.sh
+++ Exercises/working_with_compressed_files.sh
@@ -32,5 +32,7 @@ impala-shell -q "create external table if not exists large_uncompressed_table(cr
 beeline -u "jdbc:hive2://host-10-17-80-187.coe.cloudera.com:10000/default;" -n hive -p dummy -e "SET mapreduce.output.fileoutputformat.compress=true;SET hive.exec.compress.output=true;
 SET mapreduce.output.fileoutputformat.compress.codec=com.hadoop.compression.lzo.LzopCodec;
 CREATE TABLE large_compressed_table(creation_ts string, creator string, deletion_timestamp string, deletor string, subject string, predicate string, object string, language string) STORED AS INPUTFORMAT 'com.hadoop.mapred.DeprecatedLzoTextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat';
-INSERT INTO TABLE large_compressed_table SELECT * FROM large_uncompressed_table;
+INSERT INTO TABLE large_compressed_table SELECT * FROM large_uncompressed_table;"
 
+# To Index file
+#hadoop jar /usr/lib/hadoop/lib/hadoop-lzo-cdh4-0.4.15-gplextras.jar com.hadoop.compression.lzo.DistributedLzoIndexer /hdfs_location_of_table/
